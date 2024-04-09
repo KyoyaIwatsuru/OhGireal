7ae8caaf5852e25afa6ed9f74c3823c92028dad2
SaimyoArata <arata-gamba4@docomo.ne.jp>
2024-04-09 22:25:14 +0900

    refs #6: resultのページの作成

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -1,7 +1,25 @@
-export default function Page () {
+export default function result() {
   return (
-    <main>
-      <h1>パンダがカフェで注文する料理は？</h1>
-    </main>
-  )
+    <div className="flex justify-center items-center h-screen">
+      <div className="max-w-md w-full">
+      <div className="text-2xl text-center mb-8">パンダがカフェで注文する料理は？</div>
+        <div className="flex justify-between items-center">
+          <div className="text-2xl">１位の回答</div>
+          <div className="text-2xl">パンダ</div>
+        </div>
+        <div className="flex justify-between items-center mt-4">
+          <div className="text-2xl">２位の回答</div>
+          <div className="text-2xl">パンダ</div>
+        </div>
+        <div className="flex justify-between items-center mt-4">
+          <div className="text-2xl">３位の回答</div>
+          <div className="text-2xl">パンダ</div>
+        </div>
+        <div className="flex justify-between items-center mt-8">
+          <div className="text-2xl">あなたの順位</div>
+          <div className="text-2xl">５位</div>
+        </div>
+      </div>
+    </div>
+  );
 }
