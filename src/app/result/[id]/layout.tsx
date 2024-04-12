85b9a8f96a97079e4666c8d7523f76e0262e20d9
SaimyoArata <arata-gamba4@docomo.ne.jp>
2024-04-13 01:43:24 +0900

    refs #15: resultのページの修正

--- src/app/result/[id]/layout.tsx
+++ src/app/result/[id]/layout.tsx
@@ -1,4 +1,6 @@
 import { Metadata } from 'next'
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
 
 export const metadata: Metadata = {
   title: 'Result',
@@ -15,3 +17,5 @@ export default function Layout({
     </>
   )
 }
+
+config.autoAddCss = false
