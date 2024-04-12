92abacae3c5f7b5ffa611bff223b34fde0cf43a7
山崎匠真 <yamasakit3a@aquila-wl.imp>
2024-04-13 01:11:54 +0900

    ID

--- src/app/wait_answer/[id]/page.tsx
+++ src/app/wait_answer/[id]/page.tsx
@@ -1,9 +1,15 @@
 import { fetchThemes } from "@/app/lib/actions";
 import Time from "@/app/components/time";
-
+import GET_ID from "@/app/lib/page_id";
+//APIできるまでのやつ
+function CheckID(pageId:string|string[]){
+  return false;//404    
+}
 export default async function Page () {
   const themes = await fetchThemes();
-
+  const pageId = GET_ID()
+  const isIdValid= CheckID(pageId)
+  if(isIdValid==true){
   return (
     <>
       <div className="text-center mt-40 ">
@@ -18,4 +24,12 @@ export default async function Page () {
       </div>
     </>
   )
+  }else{
+    return (
+      <div>
+        <h1>404 NotFound</h1>
+        <p>ページが見つかりませんでした。</p>
+      </div>
+    );
+  }
 }
