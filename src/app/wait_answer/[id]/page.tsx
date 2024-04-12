3872e80b3c6899712138d3f1f62f6a5cdb81aae8
山崎匠真 <yamasakit3a@aquila-wl.imp>
2024-04-13 05:03:57 +0900

    wait系のフロント

--- src/app/wait_answer/[id]/page.tsx
+++ src/app/wait_answer/[id]/page.tsx
@@ -1,27 +1,31 @@
 import { fetchThemes, CheckID } from "@/app/lib/actions";
 import Time from "@/app/components/time";
+import PageId from "@/app/lib/page_id";
+//APIできるまでのやつ
+function CheckID(){
+  return true;//404    
+}
 
-export default async function Page (params: {params: { id: string }}) {
+export default async function Page ({ params }: { params: { id: string } }) {
   const themes = await fetchThemes();
-  const pageId = params.params.id;
-  const isIdValid = await CheckID(pageId);
-
-  if (isIdValid == true) {
-    return (
-      <>
-        <div className="text-center mt-40 ">
-          <div className="text-2xl">
-            <Time end_time={themes?.end_time}/>
-          </div>
+  const pageId=params.id
+  const isIdValid= CheckID()
+  if(isIdValid==true){
+  return (
+    <div className="h-screen bg-[#ffcc33]">
+      <div className="text-center  pt-[20%]">
+        <div className="text-4xl font-bold">
+          残り時間が経過するまでお待ち下さい
         </div>
-        <div className="text-center  mt-40">
-          <div className="text-2xl">
-            残り時間が経過するまでお待ち下さい
-          </div>
+      </div>
+      <div className="text-center mt-40 ">
+        <div className="text-2xl font-bold">
+          <Time end_time={themes?.end_time}/>
         </div>
-      </>
-    )
-  } else {
+      </div>
+    </div>
+  )
+  }else{
     return (
       <div>
         <h1>404 NotFound</h1>
