d32fb56f5df6fe4daa5fb953716578e73cfaef64
SaimyoArata <arata-gamba4@docomo.ne.jp>
2024-04-22 15:32:56 +0900

    refs #38: voteページの修正

--- src/app/vote/[id]/page.tsx
+++ src/app/vote/[id]/page.tsx
@@ -10,12 +10,13 @@ export default async function Page(params: {params: { id: string }}) {
 
   if (isIdValid) {
     return (
-      <div className="flex flex-col justify-center items-center bg-[#ffcc33] text-black h-screen font-bold">
+      <div className="flex flex-col justify-center items-center bg-[#ffcc33] text-black h-screen w-full font-bold px-[10%]">
         <div className="text-center mb-[5%]">
-          <div className="text-4xl">投票</div>
+          <div className="text-2xl md:text-4xl">投票</div>
         </div>
 
         <div className="text-center mb-[5%]">
+<<<<<<< HEAD
           <div className="text-2xl">
             <Time themes={themes}/>
           </div>
@@ -23,6 +24,15 @@ export default async function Page(params: {params: { id: string }}) {
 
         <div className="text-center text-2xl">面白いと思う順番にクリック</div>
         <Vote id={isIdValid.id} entries={entries} pageId={pageId} themes={themes}/>
+=======
+          <div className="text-xl md:text-2xl">
+            <Time end_time={end_time}/>
+          </div>
+        </div>
+
+        <div className="text-center text-xl md:text-2xl">面白いと思う順番にクリック</div>
+        <Vote id={1} entries={entries} pageId={pageId}/>
+>>>>>>> d005e50 (refs #38: voteページの修正)
       </div>
     );
   } else {
